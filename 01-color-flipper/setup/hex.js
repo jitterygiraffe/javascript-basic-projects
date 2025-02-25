7eac35201a1ec781c6916bfe9a8c81b7f27740b9
okwiridesigns <okwirioduor@gmail.com>
2025-02-25 23:28:41 +0100

    Update hex.js

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,4 +1,10 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hash = "#";
+function randomColor() {
+    //turning array to string
+    let newHex = hex.toString();
+
+} 
 
 //1. click button to change color randomly
 // - code to have random color changes
