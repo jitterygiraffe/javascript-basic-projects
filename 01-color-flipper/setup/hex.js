9fcdfa4d19c8ff76de984b55b580993de29d6fdc
okwiridesigns <okwirioduor@gmail.com>
2025-02-26 00:06:24 +0100

    fixed error in event listener to make button functional

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,4 +1,6 @@
-document.getElementById("btn").addEventListener("click", randomColor)
+document.getElementById("btn").addEventListener("click", ()=> {
+    document.body.style.backgroundColor = randomColor();
+});
 
 
 let hexCode = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
