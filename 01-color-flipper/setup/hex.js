70a96ce4a2f9f72e970e16553c1ecd18440f3986
okwiridesigns <okwirioduor@gmail.com>
2025-02-25 23:52:54 +0100

    debugged to make code produce random color code when function is called

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,12 +1,13 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-const hash = "#";
+
+let hexCode = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
 function randomColor() {
-    //turning array to string
-    let newHex = hex.toString();
-    for (i = 0; i<6; i++) {
-       hash += newHex[Math.floor(Math.random() * newHex.length)];
+   let hash = "#";
+   
+    for (let i = 0; i<6; i++) {
+       hash += hexCode[Math.floor(Math.random() * hexCode.length)];
     }
-    return newHex;
+     return hash;
 } ;
 
 console.log(randomColor());
