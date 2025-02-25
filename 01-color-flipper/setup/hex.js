f73ab5133a7b156d1fce04a2d885b24fb202781f
okwiridesigns <okwirioduor@gmail.com>
2025-02-25 18:10:42 +0100

    working on simple color changing app

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,6 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+//1. click button to change color randomly
+// - code to have random color changes
+//             -
+// -code for button to listen to being clicked
