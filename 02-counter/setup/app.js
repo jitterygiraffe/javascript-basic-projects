31dd65ee8f0120f939ca56bd88bea7be976a93a9
okwiridesigns <okwirioduor@gmail.com>
2025-02-26 13:51:38 +0100

    Update app.js

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -1,3 +1,26 @@
+let decreaseBtn = document.querySelector("decrease");
+let resetBtn = document.querySelector("reset");
+let increaseBtn = document.querySelector("increase");
+//initialize the counter value
+let currentValue = document.getElementById("value");
+
+decreaseBtn.addEventListener("click", function() {
+    decreasedValue = currentValue--;
+    console.log(decreasedValue);
+})
+
+// function decreaseCounter(){
+//     decreaseBtn.addEventListener("click", ()=> {
+//     decreased = currentValue--;
+//     // currentValue.innerText = decreased;
+//     console.log(decreased);
+//         }   
+//     )
+// };
+
+
+
+//function to decrease the count
 
 
 
