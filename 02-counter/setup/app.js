245f48743547876f8bc77e222da1e5652eb6eb1c
okwiridesigns <okwirioduor@gmail.com>
2025-02-26 16:49:41 +0100

    Update app.js

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -14,39 +14,3 @@ decreaseBtn.addEventListener("click", function() {
     console.log("Decreased Value:", currentValue);
 });
 
-
-// let decreaseBtn = document.querySelector(".decrease");
-// let resetBtn = document.querySelector(".reset");
-// let increaseBtn = document.querySelector(".increase");
-// //initialize the counter value
-// let current = document.querySelector(".value");
-
-
-// decreaseBtn.addEventListener("click", function() {
-//     //extract the current value
-//     let currentValue = parseInt(current.textContent);
-//     decreasedValue = currentValue--;
-    
-//     console.log(decreasedValue);
-// })
-
-// function decreaseCounter(){
-//     decreaseBtn.addEventListener("click", ()=> {
-//     decreased = currentValue--;
-//     // currentValue.innerText = decreased;
-//     console.log(decreased);
-//         }   
-//     )
-// };
-
-
-
-//function to decrease the count
-
-
-
-// each button should have an event listener 
-// that triggers certain behavior
-//1. to decrease
-//2. to reset
-//3. to increase
