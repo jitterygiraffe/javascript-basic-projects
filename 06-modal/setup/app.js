660e469611056f5ba6b6c47a71308ed083321177
okwiridesigns <okwirioduor@gmail.com>
2025-02-27 04:10:08 +0100

    added event listeners to give modal functionality

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -3,6 +3,16 @@
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
 
-let modalBtn = document.querySelector("modal-btn");
-let modalOverlay = document.querySelector("modal-overlay");
-let closeBtn = document.querySelector("close-btn");
+let modalBtn = document.querySelector(".modal-btn");
+let modalOverlay = document.querySelector(".modal-overlay");
+let closeBtn = document.querySelector(".close-btn");
+
+//adding event listeners
+modalBtn.addEventListener("click", function(){
+    modalOverlay.classList.add("open-modal");
+})
+
+//adding event listeners
+closeBtn.addEventListener("click", function(){
+    modalOverlay.classList.remove("open-modal");
+})
